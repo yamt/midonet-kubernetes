feb2b47095431b489f767009079fe7e83d4a98a4
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-03-29 15:45:52 +0900

    log

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -5,7 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 
-	kapi "k8s.io/api/core/v1"
+	// kapi "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/meta"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
@@ -22,17 +22,23 @@ func (c *Controller) Start(stopCh <-chan struct{}) {
 
 func NewController(kc *kubernetes.Clientset, resyncPeriod time.Duration) (*Controller, error) {
 	si := informers.NewSharedInformerFactory(kc, resyncPeriod)
-	si.Core().V1().Nodes().Informer().AddEventHandler(
-			cache.ResourceEventHandlerFuncs{
+	handlers := cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			node := obj.(*kapi.Node)
+			key, err := cache.MetaNamespaceKeyFunc(obj)
+			if err != nil {
+				log.WithError(err).Fatal("MetaNamespaceKeyFunc")
+			}
+			meta, err := meta.Accessor(obj)
+			if err != nil {
+				log.WithError(err).Fatal("meta.Accessor")
+			}
 			log.WithFields(log.Fields{
-				"node": node,
-			}).Debug("Add")
+				"key": key,
+				"uid": meta.GetUID(),
+				"version": meta.GetResourceVersion(),
+			}).Info("Add")
 		},
 		UpdateFunc: func(old, new interface{}) {
-			oldNode := old.(*kapi.Node)
-			newNode := new.(*kapi.Node)
 			key, err := cache.MetaNamespaceKeyFunc(old)
 			if err != nil {
 				log.WithError(err).Fatal("MetaNamespaceKeyFunc")
@@ -50,30 +56,23 @@ func NewController(kc *kubernetes.Clientset, resyncPeriod time.Duration) (*Contr
 				"uid": metaOld.GetUID(),
 				"oldVersion": metaOld.GetResourceVersion(),
 				"newVersion": metaNew.GetResourceVersion(),
-			}).Info("Add")
-			log.WithFields(log.Fields{
-				"oldNode": oldNode,
-				"newNode": newNode,
-			}).Debug("Update")
+			}).Info("Update")
 		},
 		DeleteFunc: func(obj interface{}) {
-			node, ok := obj.(*kapi.Node)
-			if !ok {
-				tombstone, ok := obj.(cache.DeletedFinalStateUnknown)
-				if !ok {
-					log.Errorf("couldn't get object from tombstone %+v", obj)
-					return
-				}
-				node, ok = tombstone.Obj.(*kapi.Node)
-				if !ok {
-					log.Errorf("tombstone contained object that is not a node %#v", obj)
-					return
-				}
+			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+			if err != nil {
+				log.WithError(err).Fatal("DeletionHandlingMetaNamespaceKeyFunc")
+			}
+			meta, err := meta.Accessor(obj)
+			if err != nil {
+				log.WithError(err).Fatal("meta.Accessor")
 			}
 			log.WithFields(log.Fields{
-				"node": node,
-			}).Debug("Delete")
+				"key": key,
+				"uid": meta.GetUID(),
+			}).Info("Delete")
 		},
-	})
+	}
+	si.Core().V1().Nodes().Informer().AddEventHandler(handlers)
 	return &Controller{informerFactory: si}, nil
 }
