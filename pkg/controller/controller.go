44ebf5c11c7d0c1531e5cb959539c9a96c145905
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-03-30 13:03:17 +0900

    controller

--- pkg/controller/controller.go
+++ pkg/controller/controller.go
@@ -32,66 +32,55 @@ func addHandler(informer cache.SharedIndexInformer, kind string) {
 	rateLimiter := workqueue.DefaultControllerRateLimiter()
 	queue := workqueue.NewNamedRateLimitingQueue(rateLimiter, kind)
 	handler := newHandler(kind, queue)
-        informer.AddEventHandler(handler)
+	informer.AddEventHandler(handler)
 }
 
 func newHandler(kind string, queue workqueue.Interface) cache.ResourceEventHandler {
 	handler := cache.ResourceEventHandlerFuncs{
 		AddFunc: func(obj interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(obj)
-			if err != nil {
-				log.WithError(err).Fatal("MetaNamespaceKeyFunc")
-			}
-			meta, err := meta.Accessor(obj)
-			if err != nil {
-				log.WithError(err).Fatal("meta.Accessor")
-			}
-			log.WithFields(log.Fields{
-				"kind": kind,
-				"key": key,
-				"uid": meta.GetUID(),
-				"version": meta.GetResourceVersion(),
-			}).Info("Add")
-			queue.Add(key)
+			logAndQueue("Delete", kind, queue, obj, nil)
 		},
 		UpdateFunc: func(old, new interface{}) {
-			key, err := cache.MetaNamespaceKeyFunc(old)
-			if err != nil {
-				log.WithError(err).Fatal("MetaNamespaceKeyFunc")
-			}
-			metaOld, err := meta.Accessor(old)
-			if err != nil {
-				log.WithError(err).Fatal("meta.Accessor for old")
-			}
-			metaNew, err := meta.Accessor(new)
-			if err != nil {
-				log.WithError(err).Fatal("meta.Accessor for new")
-			}
-			log.WithFields(log.Fields{
-				"kind": kind,
-				"key": key,
-				"uid": metaOld.GetUID(),
-				"oldVersion": metaOld.GetResourceVersion(),
-				"newVersion": metaNew.GetResourceVersion(),
-			}).Info("Update")
-			queue.Add(key)
+			logAndQueue("Delete", kind, queue, new, old)
 		},
 		DeleteFunc: func(obj interface{}) {
-			key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
-			if err != nil {
-				log.WithError(err).Fatal("DeletionHandlingMetaNamespaceKeyFunc")
-			}
-			meta, err := meta.Accessor(obj)
-			if err != nil {
-				log.WithError(err).Fatal("meta.Accessor")
-			}
-			log.WithFields(log.Fields{
-				"kind": kind,
-				"key": key,
-				"uid": meta.GetUID(),
-			}).Info("Delete")
-			queue.Add(key)
+			logAndQueue("Delete", kind, queue, obj, nil)
 		},
 	}
 	return handler
+}
+
+func logAndQueue(logMsg string, kind string, queue workqueue.Interface, obj interface{}, oldObj interface{}) error {
+	key, err := cache.DeletionHandlingMetaNamespaceKeyFunc(obj)
+	if err != nil {
+		log.WithError(err).Fatal("DeletionHandlingMetaNamespaceKeyFunc")
+	}
+	ctxLog := log.NewEntry(log.StandardLogger())
+	if _, ok := obj.(cache.DeletedFinalStateUnknown); !ok {
+		meta, err := meta.Accessor(obj)
+		if err != nil {
+			ctxLog.WithError(err).Fatal("meta.Accessor")
+		} else {
+			ctxLog = ctxLog.WithFields(log.Fields{
+				"uid": meta.GetUID(),
+				"version": meta.GetResourceVersion(),
+			})
+		}
+	}
+	if oldObj != nil {
+		metaOld, err := meta.Accessor(oldObj)
+		if err != nil {
+			ctxLog.WithError(err).Fatal("meta.Accessor")
+		}
+		ctxLog = ctxLog.WithFields(log.Fields{
+			"oldVersion": metaOld.GetResourceVersion(),
+		})
+	}
+	ctxLog = ctxLog.WithFields(log.Fields{
+		"kind": kind,
+		"key": key,
+	})
+	ctxLog.Info(logMsg)
+	queue.Add(key)
+	return nil
 }
