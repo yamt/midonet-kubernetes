220855fd4e8ddb05f589f6ba421beb63fd81472e
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-02 21:27:13 +0900

    mac

--- pkg/midonet/id.go
+++ pkg/midonet/id.go
@@ -1,6 +1,9 @@
 package midonet
 
 import (
+	"crypto/sha256"
+	"net"
+
 	"github.com/google/uuid"
 	log "github.com/sirupsen/logrus"
 )
@@ -19,4 +22,11 @@ func idForKey(key string) uuid.UUID {
 
 func subID(id uuid.UUID, s string) uuid.UUID {
 	return uuid.NewSHA1(id, []byte(s))
+}
+
+func macForKey(key string) net.HardwareAddr {
+	hash := sha256.Sum256([]byte(key))
+	// AC-CA-BA  Midokura Co., Ltd.
+	addr := [6]byte{0xac, 0xca, 0xba, hash[0], hash[1], hash[2]}
+	return net.HardwareAddr(addr[:])
 }
---
+++ pkg/midonet/id_test.go
@@ -0,0 +1,18 @@
+package midonet
+
+import (
+	"net"
+	"testing"
+	"reflect"
+)
+
+func TestMac(t *testing.T) {
+	actual := macForKey("hey")
+	expected, err := net.ParseMAC("ac:ca:ba:fa:69:0b")
+	if err != nil {
+		t.Errorf("ParseMAC")
+	}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
