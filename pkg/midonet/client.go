126cecd5dfea2c61e9f6aa2426703c44e4ee0da8
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-02 16:46:58 +0900

    log

--- pkg/midonet/client.go
+++ pkg/midonet/client.go
@@ -35,7 +35,7 @@ func Post(resource *APIResource, config *Config) error {
 	}
 	log.WithFields(log.Fields{
 		"request": request,
-		"request data": data,
+		"request-json": string(data),
 		"response": response,
 	}).Info("Do")
 	return nil
