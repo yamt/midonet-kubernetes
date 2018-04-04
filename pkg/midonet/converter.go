8554ae003fa94e3dcb25018573f04d7096f1e4ae
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-05 02:40:36 +0900

    subresource
    also call cli.Delete

--- pkg/midonet/converter.go
+++ pkg/midonet/converter.go
@@ -9,5 +9,5 @@ type Converter interface {
 	// Convert
 	// - if nil obj is given, only PathForDelete fields for the
 	//   APIResource returned are valid.
-	Convert(key string, obj interface{}, confing *Config) ([]APIResource, error)
+	Convert(key string, obj interface{}, config *Config) ([]APIResource, SubResourceMap, error)
 }
--- pkg/midonet/handler.go
+++ pkg/midonet/handler.go
@@ -4,13 +4,75 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
+type SubResource interface {
+	Convert(key string) ([]APIResource, error)
+}
+
+type SubResourceMap map[string]SubResource
+
 type Handler struct {
 	converter Converter
 	config    *Config
+	knownSubResources map[string]map[string]SubResource
 }
 
 func NewHandler(converter Converter, config *Config) *Handler {
-	return &Handler{converter, config}
+	return &Handler{converter, config, make(map[string]map[string]SubResource)}
+}
+
+func (h *Handler) deletedSubResources(key string, rs map[string]SubResource) map[string]SubResource {
+	known := h.knownSubResources[key]
+	deleted := make(map[string]SubResource)
+	for k, r := range known {
+		if _, ok := rs[k]; !ok {
+			deleted[k] = r
+		}
+	}
+	return deleted
+}
+
+func merge(a map[string]SubResource, b map[string]SubResource) map[string]SubResource {
+	c := make(map[string]SubResource)
+	for k, v := range a {
+		c[k] = v
+	}
+	for k, v := range b {
+		c[k] = v
+	}
+	return c
+}
+
+func (h *Handler) handleSubResources(key string, added map[string]SubResource, deleted map[string]SubResource, cli *Client, clog *log.Entry) error {
+	if h.knownSubResources[key] == nil {
+		h.knownSubResources[key] = make(map[string]SubResource)
+	}
+	for k, r := range merge(h.deletedSubResources(key, added), deleted) {
+		converted, err := r.Convert(k)
+		err = cli.Delete(converted)
+		if err != nil {
+			clog.WithError(err).WithFields(log.Fields{
+				"key": key,
+				"sub-key": k,
+			}).Error("failed to delete a sub resource")
+			return err
+		}
+		delete(h.knownSubResources[key], k)
+	}
+	for k, r := range added {
+		convertedSub, err := r.Convert(k)
+		err = cli.Push(convertedSub)
+		// Remember the resource regardless of err as we might have
+		// partially pushed.
+		h.knownSubResources[key][k] = r
+		if err != nil {
+			clog.WithError(err).WithFields(log.Fields{
+				"key": key,
+				"sub-key": k,
+			}).Error("failed to push a sub resource")
+			return err
+		}
+	}
+	return nil
 }
 
 func (h *Handler) Update(key string, obj interface{}) error {
@@ -18,7 +80,7 @@ func (h *Handler) Update(key string, obj interface{}) error {
 		"key": key,
 		"obj": obj,
 	})
-	converted, err := h.converter.Convert(key, obj, h.config)
+	converted, subResources, err := h.converter.Convert(key, obj, h.config)
 	if err != nil {
 		clog.WithError(err).Fatal("Failed to convert")
 	}
@@ -26,7 +88,13 @@ func (h *Handler) Update(key string, obj interface{}) error {
 	cli := NewClient(h.config)
 	err = cli.Push(converted)
 	if err != nil {
-		clog.WithError(err).Fatal("Failed to push")
+		clog.WithError(err).Error("Failed to push")
+		return err
+	}
+	err = h.handleSubResources(key, subResources, nil, cli, clog)
+	if err != nil {
+		clog.WithError(err).Error("handleSubResources")
+		return err
 	}
 	// TODO: annotate kubernetes obj
 	return nil
@@ -34,11 +102,21 @@ func (h *Handler) Update(key string, obj interface{}) error {
 
 func (h *Handler) Delete(key string) error {
 	clog := log.WithField("key", key)
-	converted, err := h.converter.Convert(key, nil, h.config)
+	converted, subResources, err := h.converter.Convert(key, nil, h.config)
 	if err != nil {
 		clog.WithError(err).Fatal("Failed to convert")
 	}
-	// TODO: Delete
 	clog.WithField("converted", converted).Info("Converted")
+	cli := NewClient(h.config)
+	err = h.handleSubResources(key, nil, subResources, cli, clog)
+	if err != nil {
+		clog.WithError(err).Error("handleSubResources")
+		return err
+	}
+	err = cli.Delete(converted)
+	if err != nil {
+		clog.WithError(err).Error("Failed to delete")
+		return err
+	}
 	return nil
 }
