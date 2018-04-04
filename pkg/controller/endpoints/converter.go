5dbc1c020144da532ad6ef53249258fa7df0924f
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-04 20:59:50 +0900

    ep chains

--- pkg/controller/endpoints/converter.go
+++ pkg/controller/endpoints/converter.go
@@ -1,6 +1,10 @@
 package endpoints
 
 import (
+	"fmt"
+
+	"k8s.io/api/core/v1"
+
 	"github.com/yamt/midonet-kubernetes/pkg/midonet"
 )
 
@@ -10,6 +14,41 @@ func newEndpointsConverter() midonet.Converter {
 	return &endpointsConverter{}
 }
 
+type endpoint struct {
+	ip       string
+	port     int
+	protocol v1.Protocol
+}
+
+func endpoints(subsets []v1.EndpointSubset) map[string][]endpoint {
+	m := make(map[string][]endpoint, 0)
+	for _, s := range subsets {
+		for _, a := range s.Addresses {
+			for _, p := range s.Ports {
+				ep := endpoint{a.IP, int(p.Port), p.Protocol}
+				l := m[p.Name]
+				l = append(l, ep)
+				m[p.Name] = l
+			}
+		}
+	}
+	return m
+}
+
 func (c *endpointsConverter) Convert(key string, obj interface{}, config *midonet.Config) ([]midonet.APIResource, error) {
-	return []midonet.APIResource{}, nil
+	resources := make([]midonet.APIResource, 0)
+	if obj != nil {
+		endpoint := obj.(*v1.Endpoints)
+		for k, eps := range endpoints(endpoint.Subsets) {
+			for _, ep := range eps {
+				epKey := fmt.Sprintf("%s/%s", key, k)
+				epChainID := midonet.IDForKey(epKey)
+				resources = append(resources, &midonet.Chain{
+					ID:   &epChainID,
+					Name: fmt.Sprintf("KUBE-SEP-%s:%s:%d:%s", epKey, ep.ip, ep.port, ep.protocol),
+				})
+			}
+		}
+	}
+	return resources, nil
 }
