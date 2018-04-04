44f638c93ce04d7d6dcec92a3ad6a1cf8bfa1227
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-04 17:43:48 +0900

    service chain

--- pkg/controller/service/converter.go
+++ pkg/controller/service/converter.go
@@ -1,6 +1,8 @@
 package service
 
 import (
+	"fmt"
+
 	"github.com/yamt/midonet-kubernetes/pkg/midonet"
 )
 
@@ -10,7 +12,17 @@ func newServiceConverter() midonet.Converter {
 	return &serviceConverter{}
 }
 
-func (c *serviceConverter) Convert(key string, obj interface{}, config *midonet.Config) ([]*midonet.APIResource, error) {
+func (_ *serviceConverter) Convert(key string, obj interface{}, config *midonet.Config) ([]*midonet.APIResource, error) {
+	serviceChainID := midonet.IDForKey(key)
 	return []*midonet.APIResource{
+		{
+			fmt.Sprintf("/chains"),
+			"",
+			fmt.Sprintf("/chains/%v", serviceChainID),
+			&midonet.Chain{
+				ID:   &serviceChainID,
+				Name: fmt.Sprintf("KUBE-SVC-%s", key),
+			},
+		},
 	}, nil
 }
