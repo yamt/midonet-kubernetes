587966a7f2fa5a4ebf3d502484ffca899d92834c
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-05 22:56:51 +0900

    implement service port changes

--- pkg/converter/service/converter.go
+++ pkg/converter/service/converter.go
@@ -18,14 +18,26 @@ func newServiceConverter() midonet.Converter {
 
 func (_ *serviceConverter) Convert(key string, obj interface{}, config *midonet.Config) ([]midonet.APIResource, midonet.SubResourceMap, error) {
 	resources := make([]midonet.APIResource, 0)
+	subs := make(midonet.SubResourceMap)
 	if obj != nil {
-		svcsChainID := converter.ServicesChainID(config)
 		svc := obj.(*v1.Service)
 		svcIP := svc.Spec.ClusterIP
 		if svc.Spec.Type != v1.ServiceTypeClusterIP || svcIP == "" || svcIP == v1.ClusterIPNone {
 			return resources, nil, nil
 		}
 		for _, p := range svc.Spec.Ports {
+			// Note: portKey format should be consistent with the
+			// endpoints converter so that it can find the right chain
+			// to add rules. (portChainID)
+			// NameSpace/Name/ServicePort.Name
+			portKey := fmt.Sprintf("%s/%s", key, p.Name)
+			portChainID := converter.IDForKey(portKey)
+			resources = append(resources, &midonet.Chain{
+				ID:       &portChainID,
+				Name:     fmt.Sprintf("KUBE-SVC-%s", portKey),
+				TenantID: config.Tenant,
+			})
+
 			var proto int
 			switch p.Protocol {
 			case "TCP":
@@ -36,28 +48,42 @@ func (_ *serviceConverter) Convert(key string, obj interface{}, config *midonet.
 				log.WithField("protocol", p.Protocol).Fatal("Unknown protocol")
 			}
 			port := int(p.Port)
-			// Note: portKey format should be consistent with the
-			// endpoints converter.
-			portKey := fmt.Sprintf("%s/%s", key, p.Name)
-			baseID := converter.IDForKey(portKey)
-			portChainID := baseID
-			jumpRuleID := converter.SubID(portChainID, "Jump to ServicePort")
-			resources = append(resources, &midonet.Chain{
-				ID:       &portChainID,
-				Name:     fmt.Sprintf("KUBE-SVC-%s", portKey),
-				TenantID: config.Tenant,
-			}, &midonet.Rule{
-				Parent:       midonet.Parent{ID: &svcsChainID},
-				ID:           &jumpRuleID,
-				DLType:       0x800,
-				NWDstAddress: svcIP,
-				NWSrcLength:  32,
-				NWProto:      proto,
-				TPDst:        &midonet.PortRange{Start: port, End: port},
-				Type:         "jump",
-				JumpChainID:  &portChainID,
-			})
+			// Use a separate key for sub resource so that those will
+			// be deleted and re-created whenever they got changed.
+			// Note that MidoNet Rules are not updateable.
+			// The above KUBE-SVC- chain is not a part of this sub resource
+			// because we want to avoid re-creating the chain itself
+			// as it would remove rules in the chain.  (Those rules are
+			// managed by the endpoints controller.)
+			portSubKey := fmt.Sprintf("%s/serviceport/%s/%d/%d", portKey, svcIP, proto, port)
+			subs[portSubKey] = &servicePort{portKey, svcIP, proto, port}
 		}
 	}
-	return resources, nil, nil
+	return resources, subs, nil
+}
+
+type servicePort struct {
+	portKey string
+	ip      string
+	proto   int
+	port    int
+}
+
+func (s *servicePort) Convert(key string, config *midonet.Config) ([]midonet.APIResource, error) {
+	svcsChainID := converter.ServicesChainID(config)
+	jumpRuleID := converter.IDForKey(key)
+	portChainID := converter.IDForKey(s.portKey)
+	return []midonet.APIResource{
+		&midonet.Rule{
+			Parent:       midonet.Parent{ID: &svcsChainID},
+			ID:           &jumpRuleID,
+			DLType:       0x800,
+			NWDstAddress: s.ip,
+			NWSrcLength:  32,
+			NWProto:      s.proto,
+			TPDst:        &midonet.PortRange{Start: s.port, End: s.port},
+			Type:         "jump",
+			JumpChainID:  &portChainID,
+		},
+	}, nil
 }
