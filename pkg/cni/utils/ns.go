50fc505db422a2e3a10a128a9354dab6f9d461f6
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-27 13:27:23 +0900

    An exported copy of getCurrentThreadNetNSPath
    From github.com/containernetworking/plugins/pkg/ns/ns_linux.go
    
    For midonet-kube-node

---
+++ pkg/cni/utils/ns.go
@@ -0,0 +1,31 @@
+// Copyright 2015-2017 CNI authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package utils
+
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/sys/unix"
+)
+
+// An exported copy of getCurrentThreadNetNSPath
+// from github.com/containernetworking/plugins/pkg/ns/ns_linux.go
+func GetCurrentThreadNetNSPath() string {
+	// /proc/self/ns/net returns the namespace of the main thread, not
+	// of whatever thread this goroutine is running on.  Make sure we
+	// use the thread's net namespace since the thread is switching around
+	return fmt.Sprintf("/proc/%d/task/%d/ns/net", os.Getpid(), unix.Gettid())
+}
