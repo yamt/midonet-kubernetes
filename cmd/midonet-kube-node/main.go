f66e233f284fe96d64d37f4112f720bf25086d7e
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-04-27 13:52:26 +0900

    Add midonet-kube-node
    Some code are taken from:
    	https://github.com/projectcalico/kube-controllers
    	https://github.com/projectcalico/cni-plugin

---
+++ cmd/midonet-kube-node/main.go
@@ -0,0 +1,98 @@
+// Copyright (c) 2017 Tigera, Inc. All rights reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"runtime"
+
+	"github.com/containernetworking/cni/pkg/types/current"
+	"github.com/projectcalico/libcalico-go/lib/logutils"
+	log "github.com/sirupsen/logrus"
+
+	k8scni "github.com/yamt/midonet-kubernetes/pkg/cni/k8s"
+	"github.com/yamt/midonet-kubernetes/pkg/cni/utils"
+	"github.com/yamt/midonet-kubernetes/pkg/config"
+	"github.com/yamt/midonet-kubernetes/pkg/converter/node"
+	"github.com/yamt/midonet-kubernetes/pkg/k8s"
+)
+
+// NOTE(yamamoto): This init function was taken from calico CNI plugin
+func init() {
+	// This ensures that main runs only on main thread (thread group leader).
+	// since namespace ops (unshare, setns) are done for a single thread, we
+	// must ensure that the goroutine does not jump from OS thread to thread
+	runtime.LockOSThread()
+}
+
+// REVISIT(yamamoto): use separate Config
+
+func main() {
+	// Configure log formatting.
+	log.SetFormatter(&logutils.Formatter{})
+
+	// Install a hook that adds file/line no information.
+	log.AddHook(&logutils.ContextHook{})
+
+	// Attempt to load configuration.
+	config := new(config.Config)
+	if err := config.Parse(); err != nil {
+		log.WithError(err).Fatal("Failed to parse config")
+	}
+	log.WithField("config", config).Info("Loaded configuration from environment")
+
+	// Set the log level based on the loaded configuration.
+	logLevel, err := log.ParseLevel(config.LogLevel)
+	if err != nil {
+		logLevel = log.InfoLevel
+	}
+	log.SetLevel(logLevel)
+
+	// Build clients to be used by the controllers.
+	k8sClientset, err := k8s.GetClient(config.Kubeconfig)
+	if err != nil {
+		log.WithError(err).Fatal("Failed to start")
+	}
+
+	nodeName := getNodeName()
+	podCIDR, err := k8scni.GetNodePodCIDR(k8sClientset, nodeName)
+
+	logger := log.WithFields(log.Fields{
+		"nodeName": nodeName,
+		"podCIDR":  podCIDR,
+	})
+	si, err := node.GetSubnetInfo(podCIDR)
+	if err != nil {
+		logger.WithError(err).Fatal("GetSubnetInfo")
+	}
+	ips := []*current.IPConfig{
+		{
+			Version: "4",
+			Address: si.NodeIP,
+			Gateway: si.GatewayIP,
+		},
+	}
+	contNetNS := utils.GetCurrentThreadNetNSPath()
+	contVethName := "midokube-node"
+	hostVethName := "midokube-mido"
+	contVethMAC, err := utils.DoNetworking(ips, contNetNS, contVethName, hostVethName, logger)
+	if err != nil {
+		logger.WithError(err).Fatal("DoNetworking")
+	}
+	logger.WithField("contVethMAC", contVethMAC).Info("Success")
+}
+
+func getNodeName() string {
+	return "k" // XXX implement
+}
