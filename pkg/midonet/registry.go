2933d907a9db65f0e1bd6542d092b662ba224f2e
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-05-14 15:22:54 +0900

    resource types registry

---
+++ pkg/midonet/registry.go
@@ -0,0 +1,35 @@
+// Copyright (C) 2018 Midokura SARL.
+// All rights reserved.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License"); you may
+//    not use this file except in compliance with the License. You may obtain
+//    a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//    License for the specific language governing permissions and limitations
+//    under the License.
+
+package midonet
+
+import (
+	"reflect"
+	"strings"
+
+	reflect2 "github.com/modern-go/reflect2"
+)
+
+func TypeNameForObject(obj interface{}) string {
+	t := reflect.TypeOf(obj)
+	fullname := t.String()
+	sep := strings.Split(fullname, ".")
+	return sep[len(sep)-1]
+}
+
+func ObjectByTypeName(name string) interface{} {
+	t := reflect2.TypeByName("midonet." + name)
+	return reflect.Zero(t.Type1()).Interface()
+}
---
+++ pkg/midonet/registry_test.go
@@ -0,0 +1,42 @@
+// Copyright (C) 2018 Midokura SARL.
+// All rights reserved.
+//
+//    Licensed under the Apache License, Version 2.0 (the "License"); you may
+//    not use this file except in compliance with the License. You may obtain
+//    a copy of the License at
+//
+//         http://www.apache.org/licenses/LICENSE-2.0
+//
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+//    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+//    License for the specific language governing permissions and limitations
+//    under the License.
+
+package midonet
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestTypeNameForObject(t *testing.T) {
+	obj := Bridge{}
+	actual := TypeNameForObject(obj)
+	expected := "Bridge"
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", string(actual), expected)
+	}
+}
+
+func TestObjectByTypeName(t *testing.T) {
+	obj := ObjectByTypeName("Bridge")
+	actual, ok := obj.(Bridge)
+	if !ok {
+		t.Errorf("wrong type %v", reflect.TypeOf(obj))
+	}
+	expected := Bridge{}
+	if actual != expected {
+		t.Errorf("got %v\nwant %v", actual, expected)
+	}
+}
