5930207aac3b8f94df0788ec2a97bb979e56bfd0
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-06-30 00:12:59 +0900

    Some tests for travis
    .travis.yml is based on the one from prometheus. [1]
    
    [1] https://github.com/prometheus/prometheus/blob/master/.travis.yml
    
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: Ib5198a85987c40b868b934e0603c8c275375420d

---
+++ tools/check-all.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+
+set -x
+set -e
+
+cd $(dirname $0)/..
+
+./tools/check-copyrights.sh
+
+dep ensure
+go test ./...
+go list ./...|grep -v github.com/midonet/midonet-kubernetes/pkg/client|xargs go vet
+GOOS=linux GOARCH=amd64 go build ./...
+GOOS=linux GOARCH=arm64 go build ./...
+GOOS=darwin GOARCH=amd64 go build ./...
