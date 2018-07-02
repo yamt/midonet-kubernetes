db759b99a9839b68cf38342507df23079c0ed116
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-07-02 22:12:11 +0900

    build.sh: Make docker account overridable
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: I763fa37e9e1033707aee883da14fb3f72aadefde

--- build.sh
+++ build.sh
@@ -3,13 +3,18 @@
 set -e
 
 TAG=$1
+DOCKERACC=$2
 
-docker build -f Dockerfile -t midonet/midonet-kube-controllers .
-docker build -f Dockerfile-node -t midonet/midonet-kube-node .
+if [ "${DOCKERACC}" = "" ]; then
+	DOCKERACC=midonet
+fi
+
+docker build -f Dockerfile -t ${DOCKERACC}/midonet-kube-controllers .
+docker build -f Dockerfile-node -t ${DOCKERACC}/midonet-kube-node .
 
 echo "Now you can tag and push images with the following commands:"
-echo "  docker tag midonet/midonet-kube-controllers midonet/midonet-kube-controllers:${TAG}"
-echo "  docker tag midonet/midonet-kube-node midonet/midonet-kube-node:${TAG}"
-echo "  docker push midonet/midonet-kube-controllers:${TAG}"
-echo "  docker push midonet/midonet-kube-node:${TAG}"
+echo "  docker tag ${DOCKERACC}/midonet-kube-controllers ${DOCKERACC}/midonet-kube-controllers:${TAG}"
+echo "  docker tag ${DOCKERACC}/midonet-kube-node ${DOCKERACC}/midonet-kube-node:${TAG}"
+echo "  docker push ${DOCKERACC}/midonet-kube-controllers:${TAG}"
+echo "  docker push ${DOCKERACC}/midonet-kube-node:${TAG}"
 
