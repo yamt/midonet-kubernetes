c85d9c24b5edcaf66e2ffd3a719a3f00e8c4d848
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-07-09 13:37:21 +0900

    build.sh: Build arm64v8 images
    Ref: MNA-1321
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: Ic7b1be005dbd9423cb69cfec890b252b0cc8d6e6

--- build.sh
+++ build.sh
@@ -9,13 +9,15 @@ if [ "${DOCKERACC}" = "" ]; then
 	DOCKERACC=midonet
 fi
 
-VARIANT=amd64-linux
-docker build -f Dockerfile-${VARIANT} -t ${DOCKERACC}/midonet-kube-controllers-${VARIANT} .
-docker build -f Dockerfile-node-${VARIANT} -t ${DOCKERACC}/midonet-kube-node-${VARIANT} .
-docker tag ${DOCKERACC}/midonet-kube-controllers-${VARIANT} ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}
-docker tag ${DOCKERACC}/midonet-kube-node-${VARIANT} ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}
+for VARIANT in amd64-linux arm64v8-linux; do
+	docker build -f Dockerfile-${VARIANT} -t ${DOCKERACC}/midonet-kube-controllers-${VARIANT} .
+	docker build -f Dockerfile-node-${VARIANT} -t ${DOCKERACC}/midonet-kube-node-${VARIANT} .
+	docker tag ${DOCKERACC}/midonet-kube-controllers-${VARIANT} ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}
+	docker tag ${DOCKERACC}/midonet-kube-node-${VARIANT} ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}
+done
 
 echo "Now you can push images with the following commands:"
-echo "  docker push ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}"
-echo "  docker push ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}"
-
+for VARIANT in amd64-linux arm64v8-linux; do
+	echo "  docker push ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}"
+	echo "  docker push ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}"
+done
