54c6db1d03ee70ca4b24655d423372b042b40a56
YAMAMOTO Takashi <yamamoto@midokura.com>
2018-07-09 13:07:18 +0900

    build.sh: Name images with GOARCH and GOOS
    Signed-off-by: YAMAMOTO Takashi <yamamoto@midokura.com>
    Change-Id: I6874eb1d96c882ec808f1d496d9557c3d0b073b8

--- build.sh
+++ build.sh
@@ -10,12 +10,12 @@ if [ "${DOCKERACC}" = "" ]; then
 fi
 
 VARIANT=amd64-linux
-docker build -f Dockerfile-${VARIANT} -t ${DOCKERACC}/midonet-kube-controllers .
-docker build -f Dockerfile-node-${VARIANT} -t ${DOCKERACC}/midonet-kube-node .
-docker tag ${DOCKERACC}/midonet-kube-controllers ${DOCKERACC}/midonet-kube-controllers:${TAG}
-docker tag ${DOCKERACC}/midonet-kube-node ${DOCKERACC}/midonet-kube-node:${TAG}
+docker build -f Dockerfile-${VARIANT} -t ${DOCKERACC}/midonet-kube-controllers-${VARIANT} .
+docker build -f Dockerfile-node-${VARIANT} -t ${DOCKERACC}/midonet-kube-node-${VARIANT} .
+docker tag ${DOCKERACC}/midonet-kube-controllers-${VARIANT} ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}
+docker tag ${DOCKERACC}/midonet-kube-node-${VARIANT} ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}
 
 echo "Now you can push images with the following commands:"
-echo "  docker push ${DOCKERACC}/midonet-kube-controllers:${TAG}"
-echo "  docker push ${DOCKERACC}/midonet-kube-node:${TAG}"
+echo "  docker push ${DOCKERACC}/midonet-kube-controllers-${VARIANT}:${TAG}"
+echo "  docker push ${DOCKERACC}/midonet-kube-node-${VARIANT}:${TAG}"
 
